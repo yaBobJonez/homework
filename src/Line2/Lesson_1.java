c0787baa73873e8de5d7aedc14967e65c6a44cd8
yaBobJonez <yaBobJonez@gmail.com>
2020-09-23 11:35:22 +0100

    23.09.2020

---
+++ src/Line2/Lesson_1.java
@@ -0,0 +1,16 @@
+package Line2;
+
+//Получить трёхзначное число от пользователя, разделить его на разряды, и перевернуть.
+
+import java.util.Scanner;
+
+public class Lesson_1 {
+    public static void main(String[] args) throws Exception {
+        int num = new Scanner(System.in).nextInt();
+        if(String.valueOf(num).length() != 3) { System.err.println("Число должно быть трёхзначным!"); System.exit(0); }
+        int a3 = num % 10;
+        int a2 = num / 10 % 10;
+        int a1 = num / 100;
+        System.out.println(String.valueOf(a3) + a2 + a1);
+    }
+}
---
+++ src/Line3/Lesson_1.java
@@ -0,0 +1,9 @@
+package Line3;
+
+public class Lesson_1 {
+    public static void main(String[] args) {
+        int res = 546;
+        char[] numerals = String.valueOf(res).toCharArray();
+        System.out.println(String.valueOf(numerals[1]) + String.valueOf(numerals[0]) + String.valueOf(numerals[2]));
+    }
+}
