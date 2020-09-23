2955c5a39073a26c6a4b2e0c9fcb493b49b9ab83
yaBobJonez <yaBobJonez@gmail.com>
2020-09-23 11:52:30 +0100

    Дополнительное задание

---
+++ src/Line4_Additional/Lesson_1.java
@@ -0,0 +1,14 @@
+package Line4_Additional;
+
+import java.util.Scanner;
+
+public class Lesson_1 {
+    public static void main(String[] args) {
+        int num = new Scanner(System.in).nextInt();
+        char[] chars = String.valueOf(num).toCharArray();
+        String rev = "";
+        for(int i = chars.length - 1; i >= 0; i--) rev += chars[i];
+        if(String.valueOf(num).equals(rev)) System.out.println("Число " + rev + " есть палиндромой.");
+        else System.out.println("Число " + rev + " НЕ есть палиндромой.");
+    }
+}
