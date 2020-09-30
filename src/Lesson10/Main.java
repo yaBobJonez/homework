d6c3f43f71c65179aaef997b9604866e3fcafbcb
yaBobJonez <yaBobJonez@gmail.com>
2020-09-30 12:05:53 +0100

    30.09.2020: Lesson 10

---
+++ src/Lesson10/Main.java
@@ -0,0 +1,23 @@
+package Lesson10;
+
+import SharedUtils.Input;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = Input.inputInt("Введите число дня недели (1-7):");
+        String s = "";
+        switch (n){
+            case 1: s = "Понедельник"; break;
+            case 2: s = "Вторник"; break;
+            case 3: s = "Среда"; break;
+            case 4: s = "Четверг"; break;
+            case 5: s = "Пятница"; break;
+            case 6: s = "Суббота"; break;
+            case 7: s = "Воскресение"; break;
+            default: System.err.println("Не день недели!"); System.exit(1);
+        }
+        System.out.println(s);
+    }
+}
--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -4,21 +4,24 @@ import SharedUtils.Input;
 
 public class Main {
     public static void main(String[] args) {
-        char[] num = Input.inputString("Введите число:").toCharArray();
-        String str = "";
-        for(char digit : num){
-            switch(digit){
-                case '0': str += "zero "; break;
-                case '1': str += "one "; break;
-                case '2': str += "two "; break;
-                case '3': str += "three "; break;
-                case '4': str += "four "; break;
-                case '5': str += "five "; break;
-                case '6': str += "six "; break;
-                case '7': str += "seven "; break;
-                case '8': str += "eight "; break;
-                case '9': str += "nine "; break;
-            }
-        } System.out.println(str);
+        do{
+            char[] num = Input.inputString("Введите число:").toCharArray();
+            String str = "";
+            for(char digit : num){
+                switch(digit){
+                    case '0': str += "zero "; break;
+                    case '1': str += "one "; break;
+                    case '2': str += "two "; break;
+                    case '3': str += "three "; break;
+                    case '4': str += "four "; break;
+                    case '5': str += "five "; break;
+                    case '6': str += "six "; break;
+                    case '7': str += "seven "; break;
+                    case '8': str += "eight "; break;
+                    case '9': str += "nine "; break;
+                    default: System.err.println("Не число или ошибка парсинга!"); System.exit(1);
+                }
+            } System.out.println(str);
+        } while(Input.inputString("Ещё раз? Y/N").equals("Y"));
     }
 }
