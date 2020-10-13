b633eb4d4a6504c9fb498b635e494fa8586fc448
yaBobJonez <yaBobJonez@gmail.com>
2020-10-13 15:13:50 +0100

    Степень 2 - Refactor

--- src/RecursionAdditional1/Main.java
+++ src/RecursionAdditional1/Main.java
@@ -4,13 +4,13 @@ import SharedUtils.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        double n = Utils.input("Введите число:").asDouble();
+        int n = Utils.input("Введите число:").asInt();
         System.out.println((isPowerOf2(n) == 1)? "Это число есть степенью двойки." : "Не являеться степенью двойки.");
     }
-    public static double isPowerOf2(double num){
-        double temp = num / 2;
-        if(temp == 1.0) return 1;
-        else if(temp < 1.0) return 0;
+    public static int isPowerOf2(int num){
+        int temp = num / 2;
+        if(temp == 1) return 0;
+        else if(temp == 2) return 1;
         else return isPowerOf2(temp);
     }
 }
