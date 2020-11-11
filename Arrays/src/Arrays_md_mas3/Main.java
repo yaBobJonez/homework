3a70fbcd53d63b1fc8d9769a38eba2cb32d59f7b
yaBobJonez <yaBobJonez@gmail.com>
2020-11-11 12:09:13 +0000

    Задачи с двухмерными массивами

---
+++ Arrays/src/Arrays_md_mas3/Main.java
@@ -0,0 +1,25 @@
+package Arrays_md_mas3;
+
+import yaBobJonez.General;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = new int[10][10];
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++) arr[i][j] = General.random(0, 25);
+        }
+        String str = "-".repeat(30) + System.lineSeparator();
+        for(int i = 0; i < 10; i++){
+            str += "| ";
+            for(int j = 0; j < 10; j++){
+                if(arr[i][j] % 2 == 0){
+                    str += arr[i][j];
+                    str += " | ";
+                }
+            }
+            str += System.lineSeparator();
+            str += "-".repeat(30) + System.lineSeparator();
+        }
+        System.out.println(str);
+    }
+}
---
+++ Arrays/src/Arrays_md_mas5/Main.java
@@ -0,0 +1,20 @@
+package Arrays_md_mas5;
+
+import yaBobJonez.General;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = new int[10][10];
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++) arr[i][j] = General.random(0, 25);
+        }
+        int[] sum = new int[10];
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                sum[i] += arr[i][j];
+            }
+        }
+        for(int j : sum) System.out.print(""+ j + " ");
+        System.out.println();
+    }
+}
