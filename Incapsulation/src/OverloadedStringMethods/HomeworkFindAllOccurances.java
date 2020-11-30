3d409acb4ce32876e4aa9ac6a0bf03a0cb86c02c
yaBobJonez <yaBobJonez@gmail.com>
2020-11-30 13:50:23 +0200

    Перегрузка методов + ДЗ

---
+++ Incapsulation/src/OverloadedStringMethods/HomeworkFindAllOccurances.java
@@ -0,0 +1,16 @@
+package OverloadedStringMethods;
+
+import yaBobJonez.Input;
+
+public class HomeworkFindAllOccurances {
+	public static void main(String[] args) {
+		String str = "This is an example string.";
+		String search = Input.input("Введите искомый символ (или строку):");
+		for(int i = 0; i < str.length(); i++){
+			int indexOfSubstring = str.indexOf(search, i);
+			if(indexOfSubstring == -1) break;
+			System.out.println(indexOfSubstring);
+			i = indexOfSubstring;
+		}
+	}
+}
---
+++ Incapsulation/src/OverloadedStringMethods/Main.java
@@ -0,0 +1,20 @@
+package OverloadedStringMethods;
+
+import yaBobJonez.Output;
+
+public class Main {
+	public static void main(String[] args) {
+		String str = "Java is not that complicated.";
+		Output.print(str.indexOf('a'));
+		Output.print(str.indexOf('a', 2));
+		Output.print(str.indexOf("not"));
+		modify(40, 34);
+		modify(56.5, 7.2);
+		modify(40, 7.2);
+		modify(56.5, 34);
+	}
+	static void modify(int a, int b){ Output.print(a+b); }
+	static void modify(double a, double b){ Output.print(a-b); }
+	static void modify(int a, double b){ Output.print(a-b); }
+	static void modify(double a, int b){ Output.print(a-b); }
+}
