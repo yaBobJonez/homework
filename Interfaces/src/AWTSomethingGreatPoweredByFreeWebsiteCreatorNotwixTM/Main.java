65bd8ce59cb092fe17c5329ecea61b950685b6f2
yaBobJonez <yaBobJonez@gmail.com>
2021-02-15 15:05:26 +0400

    Some randomness

---
+++ Interfaces/src/AWTSomethingGreatPoweredByFreeWebsiteCreatorNotwixTM/Main.java
@@ -0,0 +1,14 @@
+package AWTSomethingGreatPoweredByFreeWebsiteCreatorNotwixTM;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Frame windowByPasha = new Frame("Thomas the  tank engine is gut !11!!");
+        windowByPasha.setSize(100, 100);
+        Button yeOldeButtonInnit = new Button();
+        yeOldeButtonInnit.setBounds(10, 10, 4, 12);
+        windowByPasha.add(yeOldeButtonInnit);
+        windowByPasha.setVisible(true);
+    }
+}
