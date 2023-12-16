72af3eecbc368eb9509425e7e469fe061a49215a
yaBobJonez <yaBobJonez@gmail.com>
2023-12-16 12:20:53 +0200

    Версія .NET Framework у README (hardcoded)

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ find . -iname "out" -printf '"%p"\n' | xargs -r rm -rv
 find . -iname "cmake-build-debug" -printf '"%p"\n' | xargs -r rm -rv
 
 # Перевірити та записати поточні версії компіляторів
-sed -i "3s/.*/_Скомпільовані на C++\/GCC $(c++ -dumpfullversion), C#\/.NET Core $(dotnet --version) та Java\/OpenJDK $(java --version | head -1 | cut -f2 -d' ')._/" README.md
+sed -i "3s/.*/_Скомпільовані на C++\/GCC $(c++ -dumpfullversion), C#\/.NET Core $(dotnet --version) (C#/.NET Framework 7.0.401) та Java\/OpenJDK $(java --version | head -1 | cut -f2 -d' ')._/" README.md
 
 # Запис на GitHub репозиторій
 echo "Створити новий коміт?"
